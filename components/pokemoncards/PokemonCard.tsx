62bc9275095d2f45b90d97cfc97fde3a9c6e8817
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-22 09:12:02 +0545

    added shadow effect in cards component

--- components/pokemoncards/PokemonCard.tsx
+++ components/pokemoncards/PokemonCard.tsx
@@ -1,8 +1,32 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 export default function PokemonCard() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>Pokemon Card</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#333",
+        shadowOffset: {
+          width: 2,
+          height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+});
